b72e5cd7a30506b8585bfa4aa3cfcb7e3e45977a
MDY <MDY@DESKTOP-G64KOVT>
2017-06-16 14:23:25 +0900

    hi Mdy 메시지 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,9 @@
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hi Mdy");
-		System.out.println("Yes Mkdy");
+		System.out.println("Yes Mdy");
+		System.out.println("Yes Mky");
+		System.out.println("Yes kas");
 	}
 }
+
